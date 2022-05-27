02fb913f70add28f4785894609e28951b1a2861b
Luísa Vitória <luisa.vitoria@hotmail.com>
2022-05-27 14:34:21 -0300

    update

--- react-native/flashlight/App.js
+++ react-native/flashlight/App.js
@@ -1,25 +1,45 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import {View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Torch from "react-native-torch"
+import RNShake from 'react-native-shake'
 
 const App = () => {
   const [toggle, setToggle] = useState(false)
 
-  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle)
+  const handleChangeToggle = () => {
+   setToggle(oldToggle => !oldToggle)
+  }
+
+  useEffect(() => {
+    Torch.switchState(toggle)
+  }, [toggle])
+
+  useEffect(() => {
+    //ao chacoalhar o celular:
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle)
+    })
+
+    //essa funcao vai ser chamada quando o componente for se desmontar
+    //toda vez que add um evento, tem que remover o ouvinte dele
+    return () => subscription.remove()
+  }, [])
+  
 
   return (
     <View style={toggle ? style.containerLight : style.container}>
       <TouchableOpacity onPress={handleChangeToggle}>
-         
-      <Image
-      style={toggle ? style.lightinOn : style.lightinOff}
-      source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png') }>
-      </Image>
+      
+        <Image
+        style={toggle ? style.lightinOn : style.lightinOff}
+        source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png') }>
+        </Image>
 
-      <Image 
-      style={style.dioLogo}
-      source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png') }>
-      </Image>
-
+        <Image 
+        style={style.dioLogo}
+        source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png') }>
+        </Image>
+ 
       </TouchableOpacity>
       
 
